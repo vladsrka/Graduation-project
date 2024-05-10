3b0bf59a6704556e9d148019e4f17ba3b79fb20f
vladsrka <svv.post01@gmail.com>
2024-05-10 18:23:16 +0300

    work

--- graduation-project-1/homePage.js
+++ graduation-project-1/homePage.js
@@ -10,15 +10,33 @@ function showSlide(n) {
   });
   slides[n].style.display = 'flex'; // Показываем нужный слайд
 }
-
 function nextSlide() {
   currentSlide = (currentSlide + 1) % totalSlides; // Получаем следующий индекс слайда
   showSlide(currentSlide); // Показываем нужный слайд
 }
-
 setInterval(nextSlide, 3200); // автоматическая смена слайдов каждые 3 секунды
-
 showSlide(currentSlide); // Показываем первый слайд при загрузке страницы
 
 
+
+function changeText(link, newText) {
+  if (link.textContent === newText) {
+    link.textContent = link.getAttribute('data-default-text');
+  } else {
+    link.setAttribute('data-default-text', link.textContent);
+    link.textContent = newText;
+  }
+}
+// onclick="changeText(this, 'Ещё в разработке.')"
+
+
+
+function addStateOpenClass() {
+  const headerMobile = document.querySelector('.header-mobile');
+  headerMobile.classList.add('state-open');
+}
+function removeStateOpenClass() {
+  const headerMobile = document.querySelector('.header-mobile');
+  headerMobile.classList.remove('state-open');
+}
 

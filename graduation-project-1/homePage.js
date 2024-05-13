277fae8b25874730ad3f6c75471e7e6f2ea68b4e
vladsrka <svv.post01@gmail.com>
2024-05-13 21:12:49 +0300

    work

--- graduation-project-1/homePage.js
+++ graduation-project-1/homePage.js
@@ -121,3 +121,33 @@ document.addEventListener('click', function (event) {
 
 
 
+window.addEventListener('load', function (e) {
+  var slider = new Swiper('.promotions-slider', {
+      pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+      },
+      watchOverflow: true,
+
+      breakpoints: {
+          0: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+          },
+          576: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+          },
+          768: {
+              slidesPerView: 3,
+              spaceBetween: 25,
+          },
+          1060: {
+              slidesPerView: 4,
+              spaceBetween: 30,
+          }
+      },
+  });
+});
+
+
